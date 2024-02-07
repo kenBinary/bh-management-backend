c99355b3a701cc5ab59964da973fead2a224dad5
thennek0 <kennethbecaro0@gmail.com>
2024-02-07 22:41:21 +0800

    added transaction to remove tenant from a room

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -68,17 +68,57 @@ exports.assignRoom = [
 ];
 
 exports.freeRoom = [
-    param("roomId").isAlphanumeric().trim().escape().isLength({ min: 1 }),
-    body("tenantId").isAlphanumeric().trim().escape().isLength({ min: 1 }),
+    param("roomId").isAlphanumeric().escape().trim().isLength({ min: 1 }),
+    body("tenantId").isAlphanumeric().escape().trim().isLength({ min: 1 }),
+    body("contractId").isAlphanumeric().escape().trim().isLength({ min: 1 }),
     asyncHandler(async (req, res, next) => {
-        const roomNumber = req.params.roomId;
-        const tenantId = req.body.tenantId;
         const connection = await pool.getConnection();
-        await connection.execute("CALL p_remove_tenant_room(?,?);", [roomNumber, tenantId]);
-        connection.end();
-        res.status(200).json({
-            message: "tenant removed from room"
-        });
+        try {
+            await connection.beginTransaction();
+            const { roomId } = req.params;
+            const { tenantId, contractId } = req.body;
+
+            const tenantQuery = "update tenant set occupancy_status = false where tenant_id = ?";
+            const tenantValues = [tenantId];
+            await connection.execute(tenantQuery, tenantValues);
+
+            const contractQuery = "update contract set room_number = ? where tenant_id = ? and contract_id = ?";
+            const contractValues = [0, tenantId, contractId];
+            await connection.execute(contractQuery, contractValues);
+
+            const roomStatusQuery = "select headcount, occupant_count, room_type from room where room_number = ?"
+            const roomStatusValues = [roomId];
+            const [roomDetail] = await connection.execute(roomStatusQuery, roomStatusValues);
+            let isFull = null
+            let occupantCount = null;
+            if (roomDetail.length > 0) {
+                occupantCount = roomDetail[0].occupant_count - 1;
+                isFull = !(occupantCount < roomDetail[0].headcount);
+            }
+
+            const isOccupied = (occupantCount > 0) ? "occupied" : "vacant";
+            const roomQuery = "update room set room_status = ?, is_full = ?, occupant_count = ? where room_number = ? ";
+            const roomValues = [isOccupied, isFull, occupantCount, roomId];
+            await connection.execute(roomQuery, roomValues);
+
+            const [tenantList] = await connection.query("select tenant.tenant_id, tenant.first_name, tenant.last_name, contract.contract_id from tenant inner join contract on tenant.tenant_id = contract.tenant_id where tenant.occupancy_status = false;");
+            const [roomList] = await connection.query("select * from room");
+
+            await connection.commit();
+            res.status(200).json({
+                "message": "remove tenant success",
+                "tenantList": tenantList,
+                "roomList": roomList,
+            });
+        } catch (error) {
+            await connection.rollback();
+            res.status(400).json({
+                "message": "failed to remove tenant",
+            });
+        } finally {
+            connection.release();
+        }
+
     })
 ];
 
